b067706fd43d3edd8a8e04be45d7b00cf9c10a01
laksha08 <98178587+laksha08@users.noreply.github.com>
2026-08-04 21:53:23 -0600

    Add files via upload
    Week1 Day2

---
+++ datatypes.js
@@ -0,0 +1,33 @@
+var firstName = 'Laksha'
+console.log(firstName);
+console.log(typeof firstName);
+
+var companyName = "Test Leaf"
+console.log(companyName);
+console.log(typeof companyName);
+
+var mobileNumber = 8250000
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+
+var isAutomation = true
+console.log(isAutomation);
+console.log(typeof isAutomation);
+
+var hasPlaywright
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
+
+var name = null
+console.log(name);  
+console.log(typeof name);
+
+
+
+
+
+
+
+
+
+
