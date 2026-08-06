be8ac86eb5b0cc4bb1abf5f06f013d2c461649e3
laksha08 <98178587+laksha08@users.noreply.github.com>
2026-08-06 11:56:28 -0600

    Update Day3.js

--- Day3.js
+++ Day3.js
@@ -28,3 +28,68 @@ if (true) {
     console.log(browserVersion2)  //will print
 }
 console.log(browserVersion2)  //will print
+
+
+//Homework Number Type
+function output(a){
+if(a > 0){
+    console.log("It is a positive number");
+}
+if (a < 0) {
+    console.log("It is a negative number");
+}
+if (a === 0) {
+    console.log("It is a neutral number");
+}
+}
+output(0);
+
+//Conditional Statements
+function launchBrowser(browserName){
+        if (browserName === "Chrome"){
+            console.log("Launching Chrome Browser")
+        } else {
+            console.log("Launching " + browserName + " Browser")
+        }
+}
+ launchBrowser("Edge");
+
+ function runTests(testType){
+switch (testType) {
+    case "Smoke":
+        console.log("Running Smoke Tests");
+        break;
+    case "Sanity":
+        console.log("Running Sanity Tests");
+        break;
+    case "Regression":
+        console.log("Running Regression Tests");
+        break;
+    default:
+        console.log("Running Smoke Tests");
+        break;
+}
+ }
+runTests("Regression");
+
+//Grade Calculation
+function studentsGrade(score){
+    switch (true) {
+        case (score  >= 90 && score <= 100):
+            console.log("Grade A");
+            break;
+        case (score  >= 80 && score < 90):
+            console.log("Grade B");
+            break;
+        case (score  >= 70 && score < 80):
+            console.log("Grade C");
+            break;   
+        case (score  >= 60 && score < 70):
+            console.log("Grade D");
+            break;  
+        default:
+            console.log("Grade F");
+            break;
+    }
+}
+studentsGrade(90);
